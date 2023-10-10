5877ecca6560d0e690bcdc57c0b4edb693be59a4
Piotr Stapp <ptrstpp950@gmail.com>
2023-10-10 18:12:26 +0200

    better screenshot

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.get('/screenshot', async (req, res) => {
     const browser = await puppeteer.launch({
         headless: "new'",
         executablePath: '/usr/bin/google-chrome',
+//        executablePath: '"C:/Program Files/Google/Chrome/Application/chrome.exe"',
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
 
@@ -29,12 +30,15 @@ app.get('/screenshot', async (req, res) => {
         //height: 600, //Twitter
         height: 630, //Facebook
     });
-    await page.goto(url);
+    await page.goto(url, { waitUntil: 'load' });
     
+    const elementHandle = await page.$('#map');
+    const boundingBox = await elementHandle.boundingBox();
+
     const screenshot = await page.screenshot({
         clip: {
             x: 0,
-            y: 0,
+            y: boundingBox.y + boundingBox.height,
             width: page.viewport().width,
             height: page.viewport().height,
         }
